56bfce2b4d4c045a194b5d9c62b5ee6cb2601ca8
mput <mputilovskij@gmail.com>
2024-02-12 18:05:54 +0500

    1, 2 задача

--- hhh.py
+++ hhh.py
@@ -10,5 +10,9 @@ def index(title):
     return render_template('index.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
