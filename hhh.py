a9b425811ce3130e70f1faf02eed5066c3c278f2
mput <mputilovskij@gmail.com>
2024-02-15 17:38:09 +0500

    1, 2, 3 задачи

--- hhh.py
+++ hhh.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, redirect
 
-
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def main():
+    return render_template('base.html')
+
+
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
@@ -13,6 +19,14 @@ def index(title):
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof)
+
+
+profs = ['инженер-исполнитель', "пилот", "строитель", "экзобиолог", "врач", "инженер по терраформированию",
+         "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер жизнеобеспечения", "метеоролог",
+         "оператор марсохода", "киберинженер", "штурман", "пилот дронов"]
+@app.route('/list_prof/<list>')
+def profs_list(list):
+    return render_template('list_prof.html', list=list, profs=profs)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
